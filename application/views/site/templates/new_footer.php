4d3839441b3182f4c5d5460e97b08ae165d514e8
naimeshk <nkalariya@innovegicsolutions.in>
2019-04-12 15:29:59 +0530

    home new_header and footer or css chnages by ravi

--- application/views/site/templates/new_footer.php
+++ application/views/site/templates/new_footer.php
@@ -49,7 +49,21 @@
                 advisor regarding any questions you have including the suitability of a specific transaction.</p>
         </div>
         <div class="copy_right">
-            © 2019 - Gain Consulting
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="left_side">
+                        <ul>
+                            <li><a href="<?php echo $urlProperty.'pages/privacy-policy'; ?>">Privacy Policy</a></li>
+                            <li><a href="<?php echo $urlProperty.'pages/terms-conditions'; ?>">Terms And Conditions</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="right_side">
+                        © 2019 - Gain Consulting
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
--- application/views/site/templates/new_header.php
+++ application/views/site/templates/new_header.php
@@ -172,7 +172,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
         <div class="container">
             <a class="navbar-brand" href="<?php echo base_url(); ?>">
-                <img src="<?php echo base_url(); ?>images/logo.png" alt="logo">
+                <img src="<?php echo base_url(); ?>images/logo.png" alt="logo" style="width:114px;">
             </a>
             <button class="navbar-toggler right_bt" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
