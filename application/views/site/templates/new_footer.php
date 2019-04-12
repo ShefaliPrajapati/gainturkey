3555b5165708d661186fcf17e4404dd5444a1d46
naimeshk <nkalariya@innovegicsolutions.in>
2019-04-12 15:49:47 +0530

    style css change by ravi

--- application/views/site/templates/new_footer.php
+++ application/views/site/templates/new_footer.php
@@ -50,7 +50,7 @@
         </div>
         <div class="copy_right">
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="left_side">
                         <ul>
                             <li><a href="<?php echo $urlProperty.'pages/privacy-policy'; ?>">Privacy Policy</a></li>
@@ -58,7 +58,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="right_side">
                         © 2019 - Gain Consulting
                     </div>
