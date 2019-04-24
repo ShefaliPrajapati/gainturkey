9775962f42445d9490a20da3da9d02f93756f486
akadival <akadival@innovegicsolutions.in>
2019-04-24 12:26:42 +0530

    change in smtp mail

--- fc_smtp_settings.php
+++ fc_smtp_settings.php
@@ -1,5 +1,5 @@
 <?php 
-$config['smtp_host'] = 'smtp.gmail.com'; 
+$config['smtp_host'] = 'ssl://smtp.gmail.com'; 
 $config['smtp_port'] = '465'; 
 $config['smtp_user'] = 'dev.innovegic@gmail.com'; 
 $config['smtp_pass'] = 'esjzjhvdtzrbonep'; 
