d2ec7606b916d912efa319551f4948bf991bc5e5
Shefali <testshailesh1@gmail.com>
2019-05-14 16:17:10 +0530

    remove image issue

--- server/php/index.php
+++ server/php/index.php
@@ -11,5 +11,11 @@
  */
 
 //error_reporting(E_ALL | E_STRICT);
+if ($_GET['file'] && isset($_GET['file'])) {
+    $file = getcwd().'/'.$_GET['file'];
+    if (is_file($file)) {
+        unlink($file); // delete the file
+    }
+}
 require('UploadHandler.php');
 $upload_handler = new UploadHandler();
